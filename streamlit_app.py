24bdf90d86e24119d0ebfc32ecabad47348bd1d0
slaascode <jeffovogel@yahoo.com>
2026-01-18 14:45:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from main_agent import get_response
 
-st.titlest.titlest.title("🚀 Storage Intel Pro V2")
+st.title("📦 Storage Intel Agent")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
